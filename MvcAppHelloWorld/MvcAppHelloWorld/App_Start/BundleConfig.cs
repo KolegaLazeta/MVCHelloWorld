41e50b17173c9c58ecbf314fbdffd0e9d19afe29
Lazar Mladenovic <lazeta998@gmail.com>
2022-09-14 13:53:50 +0200

    Bootsrap update and small changes

--- MvcAppHelloWorld/MvcAppHelloWorld/App_Start/BundleConfig.cs
+++ MvcAppHelloWorld/MvcAppHelloWorld/App_Start/BundleConfig.cs
@@ -23,7 +23,7 @@ namespace MvcAppHelloWorld
                       "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap-dark.css",
                       "~/Content/site.css"));
         }
     }
